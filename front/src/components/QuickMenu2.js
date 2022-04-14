6c8d3a8cb4a86380afa73a0d3caec551198561b1
khy <khj930410@naver.com>
2022-04-15 00:16:41 +0900

    2204150016

--- front/src/components/QuickMenu2.js
+++ front/src/components/QuickMenu2.js
@@ -9,19 +9,21 @@ export default class QuickMenu2 extends Component {
     this.state={
       willOpen:false,
     }
-    
   }
 
   render() {
 
-    const openFunction= () =>{
-      this.setState({willOpen:!this.state.willOpen})
-
-
+    const openFunction= (index) =>{
+      this.setState({willOpen:!index})
+      console.log(this.state.willOpen);
       if(this.state.willOpen){
-        alert(this.state.willOpen );
+       var check = document.getElementsByClassName("QuickMenuLi")
+       console.log(check);
+       for(var i=0; i<check.length ;i++){
+        check[i].classList.add('show');
+       }
       }else{
-        alert(this.state.willOpen);
+
       }
     }
 
@@ -29,18 +31,16 @@ export default class QuickMenu2 extends Component {
 
 
     return (
-      <div className='fixed-bottom d-flex justify-content-end' id="QuickMenuWrap">
-        <button className="void me-5" id="QuickMenuButton" onClick={ e =>{ openFunction(); } }>
-          <a href='#none' className='w-100 h-100 d-flex justify-content-center align-items-center d-block'>
-          <FontAwesomeIcon icon={faPlus} className="quickbutton"/>
-          </a>
-        </button>
-        <ul className='d-flex flex-column-reverse position-relative'>
-          <li className='mt-2 QuickMenuLi'><a href='#none'><img src='/img/QuickMenu/git-hub.png'></img></a></li>
-          <li className='mt-2 QuickMenuLi'><a href='#none'><img src='/img/QuickMenu/instagram.png'></img></a></li>
-          <li className='mt-2 QuickMenuLi'><a href='#none'><img src='/img/QuickMenu/kakaotalkIcon.png'></img></a></li>
-          <li className='QuickMenuLi'><a href='#none'><img src='/img/QuickMenu/tistory.png'></img></a></li>
-        </ul>
+      <div class="d-flex flex-column-reverse" id="QuickMenuWrap">
+          <button className="d-flex align-items-center justify-content-center" id="QuickMenuButton" onClick={ e =>{ openFunction(this.state.willOpen); } }>
+            <FontAwesomeIcon icon={faPlus} className="quickbutton"/>
+          </button>
+          <ul id="QuickMenuUl"className='mb-0 d-flex flex-column-reverse position-relative p-0'>
+            <li className='mb-2 QuickMenuLi'><a href='#none'><img src='/img/QuickMenu/git-hub.png'></img></a></li>
+            <li className='mb-2 QuickMenuLi'><a href='#none'><img src='/img/QuickMenu/instagram.png'></img></a></li>
+            <li className='mb-2 QuickMenuLi'><a href='#none'><img src='/img/QuickMenu/kakaotalkIcon.png'></img></a></li>
+            <li className='mb-2 QuickMenuLi'><a href='#none'><img src='/img/QuickMenu/tistory.png'></img></a></li>
+          </ul>
       </div>
     )
   }
