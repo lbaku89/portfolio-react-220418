ac1fec1ece517a82d1a9e603b3071c4da1089159
khy <khj930410@naver.com>
2022-04-16 19:25:17 +0900

    2204161925

--- front/src/components/PreInterview.js
+++ front/src/components/PreInterview.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Accordion,AccordionItem,AccordionHeader,AccordionBody} from 'reactstrap'
+import {Accordion} from 'react-bootstrap'
 
 export default function PreInterview(){
 
-function toggle(){
-  
-}
+
 
   return (   
     <div className='khysection1'>
@@ -15,55 +13,55 @@ function toggle(){
           <h3 className='gmarket align-text-bottom'>솔직하게 적어봤어요</h3>
         </div>
         <div>
-        <Accordion
-          open="1"
-          toggle={toggle}
-        >
-          <AccordionItem>
-            <AccordionHeader targetId="1">
-              Accordion Item 1
-            </AccordionHeader>
-            <AccordionBody accordionId="1">
-              <strong>
-                This is the first item's accordion body.
-              </strong>
-              You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
-              <code>
-                .accordion-body
-              </code>
-              , though the transition does limit overflow.
-            </AccordionBody>
-          </AccordionItem>
-          <AccordionItem>
-            <AccordionHeader targetId="2">
-              Accordion Item 2
-            </AccordionHeader>
-            <AccordionBody accordionId="2">
-              <strong>
-                This is the second item's accordion body.
-              </strong>
-              You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
-              <code>
-                .accordion-body
-              </code>
-              , though the transition does limit overflow.
-            </AccordionBody>
-          </AccordionItem>
-          <AccordionItem>
-            <AccordionHeader targetId="3">
-              Accordion Item 3
-            </AccordionHeader>
-            <AccordionBody accordionId="3">
-              <strong>
-                This is the third item's accordion body.
-              </strong>
-              You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
-              <code>
-                .accordion-body
-              </code>
-              , though the transition does limit overflow.
-            </AccordionBody>
-          </AccordionItem>
+        <Accordion defaultActiveKey="">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Accordion Item #1</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+              est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Accordion Item #2</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+              est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>Accordion Item #3</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+              est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="3">
+            <Accordion.Header>Accordion Item #4</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+              est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
         </Accordion>
       </div>
       </div>
