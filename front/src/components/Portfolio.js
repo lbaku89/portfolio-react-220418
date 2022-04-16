eb3a6a733bb4f52667d0272e4145c24fe12c3de3
khy <khj930410@naver.com>
2022-04-16 13:48:20 +0900

    220416 1348

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
+import "swiper/css/pagination";
 
+
+
+import { Pagination } from "swiper";
 
 export default function Portfolio() {
   return (
@@ -16,28 +20,38 @@ export default function Portfolio() {
           <h3 className='gmarket align-text-bottom'>권현우 작업물</h3>
         </div>
       </div>
-      <Swiper
+
+      <Swiper slidesPerView={1} pagination={{clickable:true}} modules={[Pagination]} className="mySwiper">
+        <SwiperSlide>
+        <div className='container-xl'>
+          <div className="row mb-5">
+            <div className='col-12 col-md-6'>
+              <img src='/img/Porfolio/port0.png' className='d-block w-100'></img>
+            </div>
+            <div className='col-12 col-md-6' style={{"border":"1px solid black"}}>
+  
+            </div>
+          </div>
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+      </Swiper>
+
+
+       {/* <Swiper
       spaceBetween={50}
       slidesPerView={1}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
       <SwiperSlide>
-        <div className='container-lg'>
-          <div className="row">
-            <div className='col-12 col-lg-6'>
-              <img src='/img/Porfolio/port0.png' className='d-block w-100'></img>
-            </div>
-            <div className='col-12 col-lg-6' style={{"border":"1px solid black"}}>
-              <p>포트폴리오 설명</p>
-            </div>
-          </div>
-        </div>
+   
       </SwiperSlide>
       <SwiperSlide>Slide 2</SwiperSlide>
       <SwiperSlide>Slide 3</SwiperSlide>
       ...
-    </Swiper>
+    </Swiper>  */}
     </div>
   )
 }
