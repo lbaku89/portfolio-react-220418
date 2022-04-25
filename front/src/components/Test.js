20401d759266da64392ac1a444f3ba39775634b9
khw <khj930410@naver.com>
2022-04-25 18:40:47 +0900

    220425 test checkbox 구현중

--- front/src/components/Test.js
+++ front/src/components/Test.js
@@ -1,30 +1,46 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-export default function test() {
+export default function Test() {
 
 
-  
-  
+  const [message,messageUpdate]=useState();
+  const [checkedValueList,checkedValueListUpdate] = useState([]);
 
   // var [checkedSkill,checkedSkillUpdate] = useState([]);
 
   // var query = 'input[name="skill"]:checked';
   // const selectedElements = document.querySelectorAll(query);
   
-  function inputArray(){
-    
-    var isChecked = document.getElementsByClassName("skill")[0]
-    console.log(isChecked.value);
-    // checkedSkillUpdate(checkedSkill.push(this.vale));    
+  const inputCheckedValueFn = (e)=>{
+
+    var checkedValue = e.target.value
+    var isChecked = e.target.checked
+    if(isChecked){
+      checkedValueListUpdate([...checkedValueList,checkedValue]);
+    }else{
+      checkedValueListUpdate(checkedValueList.fiter( (el) => el!==checkedValue));
+    }
+
+    setTimeout(() => console.log("checkedSkillArray:"+checkedValueList), 2000);
+    messageUpdate("inputCheckedValueFn 실행완료")
+  
   }
+    
+  useEffect(inputCheckedValueFn() ,[message])
+
 
   return (  
     <div>
-      <input type="checkbox" className="skills" name="skill" value='html,css' onClick={e => {inputArray()}} placeholder="ddddddddddddddddd"/>
+      <input type="checkbox" className="skills" name="skill" value='html,css' onChange={inputCheckedValueFn()}/>
+      <input type="checkbox" className="skills" name="skill" value='jquery' onChange={inputCheckedValueFn()}/>
+      <input type="checkbox" className="skills" name="skill" value='node,mysql' onChange={inputCheckedValueFn()}/>
+      <input type="checkbox" className="skills" name="skill" value='react' onChange={inputCheckedValueFn()}/>
+
       {/* <input type="checkbox" class="skills" name="skill" value='jquery' onclick={inputArray()}>jquery</input>
       <input type="checkbox" class="skills" name="skill" value='node,mysql' onclick={inputArray()}>node,mysql</input>
       <input type="checkbox" class="skills" name="skill" value='react' onclick={inputArray()}>react</input> */}
+      <p>{message}</p>
     </div>
   )
 }
